be4acd5527098c6186838c5f778268652147bd5c
Bear <bear@pop-os.localdomain>
2021-05-05 16:23:49 +0300

    added imports to the test

---
+++ src/test/java/dao/UsersDaoTest.java
@@ -0,0 +1,13 @@
+package dao;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.*;
+import org.junit.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import static org.junit.Assert.*;
+
+public class UsersDaoTest {
+
+
+}

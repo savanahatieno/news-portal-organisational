d1de36e37d411fe5c5535fcd754ff104395fa386
Bear <bear@pop-os.localdomain>
2021-05-05 16:33:19 +0300

    added more test

--- src/test/java/dao/UsersDaoTest.java
+++ src/test/java/dao/UsersDaoTest.java
@@ -9,5 +9,21 @@ import static org.junit.Assert.*;
 
 public class UsersDaoTest {
 
+    @Test
+    public void addedUserReturned_Correctly(){
+        Users users = setUpNewUser();
+        sql2oUsersDao.add(user);
+        assertEquals(user.getName(), sql20UsersDao.findById(user.getId()).getName())
+    }
+
+
+    @Test
+    public void addingUserToDbSetsUserId() {
+        Users user = setUpNewUser();
+        int originalId= user.getId();
+        sql2oUsersDao.add(user);
+        assertNotEquals(originalId,user.getId());
+    }
+
 
 }

0a70aa1281bc8f6d5df36fa9e0a530ca5e9b618a
Bear <bear@pop-os.localdomain>
2021-05-06 12:25:21 +0300

    created a heroku link for localhost

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,9 @@
+mport com.google.gson.Gson;
+import dao.Sql2oDepartmentsDao;
+import dao.Sql2oNewsDao;
+import dao.Sql2oUsersDao;
+import exceptions.ApiException;
+
 import models.Departments;
 import models.News;
 import models.Users;
@@ -14,5 +20,21 @@ import static spark.Spark.staticFileLocation;
 
 
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+    public static void main(String[] args) {
+
+        port(getHerokuAssignedPort());
+        Sql2oNewsDao sql2oNewsDao;
+        Sql2oUsersDao sql2oUsersDao;
+        Sql2oDepartmentsDao sql2oDepartmentsDao;
+        Connection conn;
+        Gson gson = new Gson();
+        staticFileLocation("/public");
 
 }

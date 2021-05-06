71e762b466f71d3aface9a8050a579dae18b0a16
Bear <bear@pop-os.localdomain>
2021-05-06 12:24:13 +0300

    added import to the app.java

---
+++ src/main/java/App.java
@@ -0,0 +1,18 @@
+import models.Departments;
+import models.News;
+import models.Users;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.*;
+import static spark.Spark.staticFileLocation;
+
+
+public class App {
+
+}
